262172b1eae95a52298d87de0507f93d98851bbb
Jose Sentis <jose.sentis1987@gmail.com>
2019-04-03 11:46:41 +0200

    Adds new features for CSS

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -1,10 +1,39 @@
 import gulp from 'gulp';
-import rename from 'gulp-rename'; 
+import cssnano from 'gulp-cssnano';
+import autoprefixer from 'gulp-autoprefixer';
+import rename from 'gulp-rename';
+import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+
+const routes = {
+  src: {
+    styles: 'src/assets/scss'
+  },
+  dest: {
+    styles: 'dist/assets/css'
+  }
+};
+
+const SASS_FILES = `${routes.src.styles}/**/*.scss`;
+const AUTOPREFIXER_ARGS = {
+  browsers: ['last 2 versions'],
+  cascade: false
+};
 
 gulp.task('default', function() {
-    console.log('execute gulp file');
-
-    return gulp.src('src/assets/scss/styles.scss')
-        .pipe(rename('css/styles.css'))
-        .pipe(gulp.dest('dist'));
+  return gulp
+    .src(SASS_FILES)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer(AUTOPREFIXER_ARGS))
+    .pipe(
+      cssnano({
+        discardComments: { removeAll: true },
+        reduceIndents: false,
+        zIndex: false
+      })
+    )
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(routes.dest.styles));
 });
