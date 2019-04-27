88ea8731b858910cfe132711152893dea7c5a5d5
Jose Sentis <jose.sentis1987@gmail.com>
2019-04-27 18:48:36 +0200

    Removes logs

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -2,10 +2,6 @@ import gulp, { watch } from 'gulp';
 
 import styles, { SASS_FILES } from './tasks/styles';
 
-console.log(SASS_FILES);
-
-// const WATCH_OPTIONS = { usePolling: true };
-
 const watcher = () => {
     watch(SASS_FILES, styles);
 };
