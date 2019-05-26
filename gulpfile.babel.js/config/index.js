1e2dfd6c243124d3728bf19ed2df90055615c6e3
Jose Sentis <jose.sentis1987@gmail.com>
2019-05-26 23:59:53 +0200

    Iterates CRP to compile on views folder

--- gulpfile.babel.js/config/index.js
+++ gulpfile.babel.js/config/index.js
@@ -1,5 +1,7 @@
 const SRC_ASSETS = 'src/assets';
-const DEST_ASSETS = 'dist/assets';
+
+const DEST = 'dist';
+const DEST_ASSETS = `${DEST}/assets`;
 
 const routes = {
   src: {
@@ -7,12 +9,14 @@ const routes = {
     images: `${SRC_ASSETS}/img`,
     styles: `${SRC_ASSETS}/scss`,
     scripts: `${SRC_ASSETS}/js`,
+    views: `${SRC_ASSETS}/views`,
   },
   dest: {
     fonts: `${DEST_ASSETS}/fonts`,
     images: `${DEST_ASSETS}/img`,
     styles: `${DEST_ASSETS}/css`,
     scripts: `${DEST_ASSETS}/js`,
+    views: DEST,
   },
 };
 
--- gulpfile.babel.js/tasks/styles.base.js
+++ gulpfile.babel.js/tasks/styles.base.js
@@ -40,7 +40,7 @@ const styles = () =>
     .pipe(gulpIf(function (file) {
       return file.path.includes('crp');
     },
-      gulp.dest(`${routes.dest.styles}/crp`),
+      gulp.dest(routes.src.views),
       gulp.dest(routes.dest.styles)
     ));
 
