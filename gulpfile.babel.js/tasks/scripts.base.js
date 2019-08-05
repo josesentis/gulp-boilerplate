86e0390af1069e94d97ae01c5261f074ae23ff4e
Jose Sentis <jose.sentis1987@gmail.com>
2019-08-05 19:54:26 +0200

    Changes babelify for gulp plugin gulp-babelify

--- gulpfile.babel.js/tasks/scripts.base.js
+++ gulpfile.babel.js/tasks/scripts.base.js
@@ -1,25 +1,23 @@
-import babelify from 'babelify';
-import browserify from 'browserify';
+import browserify from 'gulp-browserify';
 import gulp from 'gulp';
 import rename from 'gulp-rename';
 import plumber from 'gulp-plumber';
 import size from 'gulp-size';
 import uglify from 'gulp-uglify';
 import sourcemaps from 'gulp-sourcemaps';
-import source from 'vinyl-source-stream';
-import buffer from 'vinyl-buffer';
 
 import { routes } from '../config';
 
 const SCRIPTS_FILES = `${routes.src.scripts}/**/*.js`;
-const FILE_NAME = 'app.js';
+const SCRIPTS_SRC = `${routes.src.scripts}/*.js`;
 
-const scripts = () => {
-  return browserify({ entries: `${routes.src.scripts}/${FILE_NAME}` })
-    .transform(babelify.configure({ presets: ['@babel/env'] }))
-    .bundle()
-    .pipe(source(FILE_NAME))
-    .pipe(buffer())
+const scripts = () =>
+  gulp
+    .src(SCRIPTS_SRC)
+    .pipe(browserify({
+      transform: ['babelify'],
+      extensions: ['.js']
+    }))
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(uglify())
@@ -28,7 +26,6 @@ const scripts = () => {
     .pipe(plumber.stop())
     .pipe(size())
     .pipe(gulp.dest(routes.dest.scripts));
-}
 
 export { SCRIPTS_FILES };
 export default scripts;
